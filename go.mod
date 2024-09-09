910e4e618b729d686a3b5a9b553ea506433e8918
bdronneau <basti1.dr@gmail.com>
2024-09-09 11:00:13 +0200

    WIP

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/bdronneau/memoriesbox
 
-go 1.21
+go 1.22.0
 
 require (
 	github.com/DATA-DOG/go-sqlmock v1.5.2
@@ -17,13 +17,24 @@ require (
 	github.com/volatiletech/randomize v0.0.1
 	github.com/volatiletech/sqlboiler/v4 v4.16.2
 	github.com/volatiletech/strmangle v0.0.6
+	gitlab.com/greyxor/slogor v1.2.11
+	go.opentelemetry.io/contrib/instrumentation/github.com/labstack/echo/otelecho v0.54.0
+	go.opentelemetry.io/otel v1.29.0
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.29.0
+	go.opentelemetry.io/otel/sdk v1.29.0
 	go.uber.org/mock v0.4.0
 )
 
 require (
+	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/gofrs/uuid v4.2.0+incompatible // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.22.0 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
@@ -54,15 +65,24 @@ require (
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	github.com/volatiletech/inflect v0.0.1 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.29.0 // indirect
+	go.opentelemetry.io/otel/metric v1.29.0 // indirect
+	go.opentelemetry.io/otel/trace v1.29.0 // indirect
+	go.opentelemetry.io/proto/otlp v1.3.1 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
-	golang.org/x/crypto v0.22.0 // indirect
+	golang.org/x/crypto v0.26.0 // indirect
 	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
-	golang.org/x/net v0.24.0 // indirect
-	golang.org/x/sync v0.6.0 // indirect
-	golang.org/x/sys v0.19.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/net v0.28.0 // indirect
+	golang.org/x/sync v0.8.0 // indirect
+	golang.org/x/sys v0.24.0 // indirect
+	golang.org/x/text v0.17.0 // indirect
+	golang.org/x/time v0.6.0 // indirect
 	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20240822170219-fc7c04adadcd // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240822170219-fc7c04adadcd // indirect
+	google.golang.org/grpc v1.65.0 // indirect
+	google.golang.org/protobuf v1.34.2 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- pkg/web/main.go
+++ pkg/web/main.go
@@ -6,12 +6,24 @@ import (
 	"fmt"
 	"html/template"
 	"io/fs"
+	"log"
 	"log/slog"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 
 	"github.com/bdronneau/memoriesbox/pkg/logger"
 	"github.com/bdronneau/memoriesbox/pkg/repositories"
+	"gitlab.com/greyxor/slogor"
+	"go.opentelemetry.io/contrib/instrumentation/github.com/labstack/echo/otelecho"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/resource"
+	"go.opentelemetry.io/otel/sdk/trace"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 
 	"github.com/labstack/echo/v4"
 )
@@ -30,6 +42,8 @@ type app struct {
 	listenAddress string
 
 	ExtraLog bool
+
+	tracerProvider *sdktrace.TracerProvider
 }
 
 type Config struct {
@@ -37,6 +51,10 @@ type Config struct {
 	debug         *bool
 	port          *int
 	featAddMemory *bool
+
+	tracingName     *string
+	tracingEnabled  *bool
+	tracingOtelRate *string
 }
 
 func GetConfig(fs *flag.FlagSet) Config {
@@ -45,6 +63,10 @@ func GetConfig(fs *flag.FlagSet) Config {
 		debug:         fs.Bool("api-debug", false, "API Debug mode"),
 		port:          fs.Int("api-port", 1080, "API Port"),
 		featAddMemory: fs.Bool("feat-add-memory", true, "Interface to add memory"),
+
+		tracingEnabled:  fs.Bool("tracing-enabled", false, "Enable tracing"),
+		tracingName:     fs.String("tracing-name", "memoriesbox", "Server name for tracing"),
+		tracingOtelRate: fs.String("tracing-rate", "always", "Which rate for tracing"),
 	}
 }
 
@@ -68,6 +90,15 @@ func New(config Config, fs fs.FS, loggerApp logger.App, repoApp repositories.App
 
 	app.echo = app.ConfigureEcho(*config.debug, fs, *config.featAddMemory)
 
+	if *config.tracingEnabled {
+		tracerProvider, err := app.InitTracer(*config.tracingName, *config.tracingOtelRate)
+		if err != nil {
+			slog.Error("Can not init tracer", slogor.Err(err))
+			os.Exit(1)
+		}
+		app.tracerProvider = tracerProvider
+	}
+
 	return app
 }
 
@@ -76,6 +107,12 @@ func (a *app) Done() <-chan struct{} {
 }
 
 func (a *app) Start() {
+	defer func() {
+		if err := a.tracerProvider.Shutdown(context.Background()); err != nil {
+			log.Printf("Error shutting down tracer provider: %v", err)
+		}
+	}()
+
 	if err := a.echo.Start(a.listenAddress); err != nil && err != http.ErrServerClosed {
 		a.echo.Logger.Fatalf("shutting down api server: ", err)
 	}
@@ -105,9 +142,11 @@ func (a *app) ConfigureEcho(debug bool, embedFs fs.FS, featAddMemory bool) *echo
 
 	fsys, err := fs.Sub(embedFs, "static")
 	if err != nil {
-		slog.Error("Can not read ")
+		slog.Error("Can not read", slogor.Err(err))
 		os.Exit(1)
 	}
+
+	e.Use(otelecho.Middleware("memoriesbox"))
 
 	e.GET("/probes/ready", a.readyHandler)
 	e.GET("/probes/live", a.liveHandler)
@@ -129,4 +168,90 @@ func (a *app) ConfigureEcho(debug bool, embedFs fs.FS, featAddMemory bool) *echo
 
 func (a *app) GetEcho() *echo.Echo {
 	return a.echo
+}
+
+func (a *app) InitTracer(name string, sampler string) (*sdktrace.TracerProvider, error) {
+	ctx := context.Background()
+
+	otelServiceName, bool := os.LookupEnv("OTEL_SERVICE_NAME")
+	if bool {
+		name = otelServiceName
+	}
+
+	otelResource, err := newResource(ctx, name)
+	if err != nil {
+		return nil, fmt.Errorf("resource: %w", err)
+	}
+
+	tracerExporter, err := newTraceExporter(ctx, os.Getenv("OTEL_EXPORTER_OTLP_ENDPOINT"))
+	if err != nil {
+		return nil, fmt.Errorf("trace exporter: %w", err)
+	}
+
+	otelServiceSampler, bool := os.LookupEnv("OTEL_TRACES_SAMPLER")
+	if bool {
+		sampler = otelServiceSampler
+	}
+
+	otelSampler, err := newSampler(strings.TrimSpace(sampler))
+	if err != nil {
+		return nil, fmt.Errorf("sampler: %w", err)
+	}
+
+	tracerProvider := trace.NewTracerProvider(
+		trace.WithBatcher(tracerExporter),
+		trace.WithResource(otelResource),
+		trace.WithSampler(otelSampler),
+	)
+
+	otel.SetTracerProvider(tracerProvider)
+	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}, propagation.Baggage{}))
+
+	return tracerProvider, nil
+}
+
+func newResource(ctx context.Context, serviceName string) (*resource.Resource, error) {
+	newResource, err := resource.New(ctx,
+		resource.WithFromEnv(),
+		resource.WithAttributes(
+			// semconv.ServiceVersion(model.Version()),
+			// attribute.String("git.commit.sha", model.GitSha()),
+			attribute.String("service.name", serviceName),
+		),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("create resource: %w", err)
+	}
+
+	r, err := resource.Merge(resource.Default(), newResource)
+	if err != nil {
+		return nil, fmt.Errorf("merge resource with default: %w", err)
+	}
+
+	return r, nil
+}
+
+func newSampler(rate string) (trace.Sampler, error) {
+	switch rate {
+	case "always":
+		return trace.AlwaysSample(), nil
+
+	case "never":
+		return trace.NeverSample(), nil
+
+	default:
+		rateRatio, err := strconv.ParseFloat(rate, 64)
+		if err != nil {
+			return nil, fmt.Errorf("parse sample rate `%s`: %w", rate, err)
+		}
+
+		return trace.TraceIDRatioBased(rateRatio), nil
+	}
+}
+
+func newTraceExporter(ctx context.Context, endpoint string) (trace.SpanExporter, error) {
+	return otlptracegrpc.New(ctx,
+		otlptracegrpc.WithInsecure(),
+		otlptracegrpc.WithEndpoint(endpoint),
+	)
 }
